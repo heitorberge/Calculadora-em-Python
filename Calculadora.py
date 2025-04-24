c8dbede426fbbb0904f006d71da7540b4d81fb27
Heitor Berg <heitorberg114@gmail.com>
2025-04-24 09:27:44 -0300

    Add files via upload

---
+++ Calculadora.py
@@ -0,0 +1,15 @@
+print('Calculadora!!!')
+n1 = int(input('Digite um Valor pra Calculadora: '))
+print('|/|*|-|+|')
+s = str(input('Digite um símbolo pra Calculadora: '))
+n2 = int(input('Digite um Valor pra Calculadora: '))
+res = 0
+if s == '/':
+    res =  n1 / n2
+if s == '*':
+    res =  n1 * n2
+if s == '-':
+    res =  n1 - n2
+if s == '+':
+    res =  n1 + n2
+print('{} {} {} = {}'.format(n1,s,n2,res))
